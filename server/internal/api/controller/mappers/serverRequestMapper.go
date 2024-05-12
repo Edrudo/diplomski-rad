b5050a5fe141e425270140b8b6e47fca33a2a7e9
eduardduras <eduard.duras@axilis.com>
2024-05-12 19:55:37 +0200

    changed dto

--- server/internal/api/controller/mappers/serverRequestMapper.go
+++ server/internal/api/controller/mappers/serverRequestMapper.go
@@ -13,7 +13,7 @@ func NewServerRequestMapper() *ServerRequestMapper {
 
 func (m *ServerRequestMapper) MapPartDtoToPartDomainModel(part controller.Part) models.Part {
 	return models.Part{
-		DataHash:   part.ImageHash,
+		DataHash:   part.DataHash,
 		PartNumber: part.PartNumber,
 		TotalParts: part.TotalParts,
 		PartData:   part.PartData,
