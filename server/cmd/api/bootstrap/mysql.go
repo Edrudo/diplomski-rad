fb3a0151162e94763c8cf456a7adebf0142722cf
eduardduras <eduard.duras@axilis.com>
2024-06-17 20:36:50 +0200

    credentials

--- server/cmd/api/bootstrap/mysql.go
+++ server/cmd/api/bootstrap/mysql.go
@@ -3,8 +3,8 @@ package bootstrap
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
-	"github.com/cenkalti/backoff"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/pkg/errors"
 	"go.uber.org/zap"
@@ -18,40 +18,28 @@ const (
 
 func newMysqlConnection(logger *zap.Logger) *sql.DB {
 	var sqlDb *sql.DB
-	err := backoff.Retry(
-		func() error {
-
-			db, err := sql.Open(
-				"mysql",
-				fmt.Sprintf(
-					mysqlConnectionFormat,
-					config.Cfg.MysqlConfig.Username,
-					config.Cfg.MysqlConfig.Password,
-					config.Cfg.MysqlConfig.DatabaseAddress,
-					config.Cfg.MysqlConfig.DatabaseName,
-				),
-			)
-			if err != nil {
-				logger.Warn(errors.Wrap(err, "cannot connect to mysql DB").Error())
-				return err
-			}
-
-			err = db.Ping()
-			if err != nil {
-				logger.Warn(errors.Wrap(err, "mysql DB ping failed").Error())
-				return err
-			}
-
-			sqlDb = db
-			logger.Info("connected to mysql DB successfully")
-			return nil
-		},
-		backoff.NewExponentialBackOff(),
+	db, err := sql.Open(
+		"mysql",
+		fmt.Sprintf(
+			mysqlConnectionFormat,
+			config.Cfg.MysqlConfig.Username,
+			config.Cfg.MysqlConfig.Password,
+			config.Cfg.MysqlConfig.DatabaseAddress,
+			config.Cfg.MysqlConfig.DatabaseName,
+		),
 	)
 	if err != nil {
+		logger.Warn(errors.Wrap(err, "cannot connect to mysql DB").Error())
 		panic(err)
 	}
 
-	return sqlDb
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	}
 
+	sqlDb = db
+	logger.Info("connected to mysql DB successfully")
+
+	return sqlDb
 }
